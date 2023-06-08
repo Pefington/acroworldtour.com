d543ba040a7422424bfc260c01ed038867d07c61
Pierre-François Salmand <maabaa@gmail.com>
2023-06-09 03:54:16 +0700

    [layout] Remove <main/> from layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import "semantic-ui-flag/flag.min.css";
 
-import classNames from "classnames";
 import type { AppProps } from "next/app";
 import { Exo } from "next/font/google";
 import { SWRConfig } from "swr";
@@ -15,19 +14,11 @@ const font = Exo({
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout fontClass={font.className}>
-      <main
-        className={classNames(
-          font.className,
-          "flex w-full flex-col items-center overflow-y-scroll pb-40 pt-4 sm:px-2",
-          "lg:pb-4 lg:pl-52 lg:pr-4 lg:pt-28",
-        )}
-      >
-        <SWRConfig value={{ fetcher }}>
-          <Component {...pageProps} />
-        </SWRConfig>
-      </main>
-    </Layout>
+    <SWRConfig value={{ fetcher }}>
+      <Layout fontClass={font.className}>
+        <Component {...pageProps} className={font.className} />
+      </Layout>
+    </SWRConfig>
   );
 };
 
