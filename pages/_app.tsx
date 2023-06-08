e601220610216f690aa26170fab984035d319499
Pierre-François Salmand <maabaa@gmail.com>
2023-06-09 05:39:17 +0700

    [layout] Bring main back to layout to pass font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "semantic-ui-flag/flag.min.css";
 
+import classNames from "classnames";
 import type { AppProps } from "next/app";
 import { Exo } from "next/font/google";
 import { SWRConfig } from "swr";
@@ -16,7 +17,9 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SWRConfig value={{ fetcher }}>
       <Layout fontClass={font.className}>
-        <Component {...pageProps} className={font.className} />
+        <main className={classNames(font.className, "w-full")}>
+          <Component {...pageProps} />
+        </main>
       </Layout>
     </SWRConfig>
   );
