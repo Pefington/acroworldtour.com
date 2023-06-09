e9d2d41cc29ab9906c9f5dafae799aa3dad48781
Pierre-François Salmand <maabaa@gmail.com>
2023-06-09 23:46:35 +0700

    [head] Add metadata

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -27,7 +27,50 @@ const Layout = ({ children, fontClass }: LayoutProps) => {
     >
       <Head>
         <title>{pageTitle}</title>
+
         <meta name="description" content={pageDescription} />
+
+        <meta
+          name="Generator"
+          content="Acro World Tour - Copyright (C). All rights reserved."
+        />
+        <meta name="author" content="Acro World Tour" />
+        <meta
+          name="copyright"
+          content="Copyright &copy; Acro World Tour, All Rights Reserved"
+        />
+        <meta name="reply-to" content="info[at]acroworldtour.com" />
+        <meta
+          name="Keywords"
+          content="Acro World Tour - The world's best pilots fighting for the most prestigious title"
+        />
+        <meta
+          name="Description"
+          content="The world's best pilots fighting for the most prestigious title"
+        />
+
+        {/* Favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Nav fontClass={fontClass} activeNav={activeNav} />
       {children}
