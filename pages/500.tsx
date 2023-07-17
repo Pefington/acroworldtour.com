1d3b24ef2b2dfc6857a1711ca9ea0191151394f2
Pierre-François Salmand <maabaa@gmail.com>
2023-07-17 12:31:52 +0700

    Add 500 status page

---
+++ pages/500.tsx
@@ -0,0 +1,20 @@
+import cn from "classix";
+
+const Custom500 = () => {
+  return (
+    <h2
+      className={cn(
+        "bg-[url('/img/404.jpg')] bg-cover bg-center bg-no-repeat",
+        "w-screen grow",
+        "flex items-center justify-center",
+        "text-lg font-bold",
+        "sm:text-xl",
+      )}
+    >
+      500 <div className="mx-3 h-12 w-[2px] bg-primary/50" /> There was an error
+      on our side. Please try again later.
+    </h2>
+  );
+};
+
+export default Custom500;
