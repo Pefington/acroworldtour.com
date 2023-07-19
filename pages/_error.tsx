fd9f89b2706a84cbb350d0a724a00966b1ca5222
Pierre-François Salmand <maabaa@gmail.com>
2023-07-19 12:39:59 +0700

    Make custom error page

---
+++ pages/_error.tsx
@@ -0,0 +1,38 @@
+import cn from "classix";
+import { NextPageContext } from "next/types";
+
+interface Props {
+  statusCode?: number;
+}
+
+const Error = ({ statusCode }: Props) => {
+  return (
+    <h2
+      className={cn(
+        "w-screen grow",
+        "flex items-center justify-center",
+        "text-lg font-bold",
+        "sm:text-xl",
+      )}
+    >
+      {statusCode
+        ? `${statusCode} ${(
+            <div className="mx-3 h-12 w-[2px] bg-primary/50" />
+          )} An error occurred on the server.`
+        : "An error occurred on client"}
+    </h2>
+  );
+};
+
+Error.getInitialProps = ({
+  res,
+  err,
+}: {
+  res: NextPageContext["res"];
+  err: NextPageContext["err"];
+}) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
