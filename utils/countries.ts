8f98ca7769478fc6ef89c43e8c2a9b2b669a2cdb
Pierre-François Salmand <maabaa@gmail.com>
2023-07-25 15:53:17 +0700

    Add optional chaining to getCountryName()

--- utils/countries.ts
+++ utils/countries.ts
@@ -9,7 +9,7 @@ countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
 export const getCountryCode = (country: string | undefined) => {
   if (country?.length === 3)
     return alpha3ToAlpha2(country.toUpperCase()).toLowerCase();
-  return country ? getAlpha2Code(country, 'en').toLowerCase() : 'earth';
+  return country ? getAlpha2Code(country, 'en')?.toLowerCase() : 'earth';
 };
 
 export const getCountryName = (country: string | undefined) =>
